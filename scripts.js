2fce2e9af70ee3471b4f3a44117d223ef241fa4b
Brayan Lopes <brylps90@outlook.com>
2025-10-08 17:33:42 -0300

    Added function to the button

--- scripts.js
+++ scripts.js
@@ -1,3 +1,6 @@
+const form = document.querySelector("form")
+const inputForm = document.getElementById("itemAdded")
+const buttonForm = document.getElementById("buttonAddItem")
 const item = document.querySelector("ul")
 const itemStructureLi = document.createElement("li")
 const itemStructureDiv = document.createElement("div")
@@ -12,3 +15,27 @@ itemStructureDiv.classList.add("textItem")
 
 // Searching for the image(trash) to put on the button
 itemStructureImg.src = "./assets/icons/trash.svg"
+
+// Changing the input type
+itemStructureInput.type = "checkbox"
+
+function addNewItem() {
+    // Showing elements in the list
+    item.append(itemStructureLi)
+
+    itemStructureLi.append(itemStructureDiv)
+    itemStructureDiv.append(itemStructureInput)
+    itemStructureDiv.append(itemName)
+
+    itemStructureLi.append(itemStructureButton)
+    itemStructureButton.append(itemStructureImg)
+
+    itemName.textContent = inputForm.value
+}
+
+
+buttonAddItem.onclick = (event) => {
+    event.preventDefault()
+    
+    addNewItem()
+}
