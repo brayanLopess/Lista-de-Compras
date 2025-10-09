0f3b2d59999bb320f0203532199180f60387ef19
Brayan Lopes <brylps90@outlook.com>
2025-10-09 17:01:18 -0300

    Event to add items to the list created

--- scripts.js
+++ scripts.js
@@ -1,28 +1,31 @@
+// Declaring vars
 const form = document.querySelector("form")
-const inputForm = document.getElementById("itemAdded")
-const buttonForm = document.getElementById("buttonAddItem")
 const item = document.querySelector("ul")
-const itemStructureLi = document.createElement("li")
-const itemStructureDiv = document.createElement("div")
-const itemStructureInput = document.createElement("input")
-const itemName = document.createElement("span")
-const itemStructureButton = document.createElement("button")
-const itemStructureImg = document.createElement("img")
 
-// Applying classes in the items
-itemStructureLi.classList.add("listItem")
-itemStructureDiv.classList.add("textItem")
+// Event to add items to the list
+form.addEventListener("submit", (event) => {
+    event.preventDefault()
 
-// Searching for the image(trash) to put on the button
-itemStructureImg.src = "./assets/icons/trash.svg"
+    const inputForm = document.getElementById("itemAdded")
+    const buttonForm = document.getElementById("buttonAddItem")
+    const itemStructureLi = document.createElement("li")
+    const itemStructureDiv = document.createElement("div")
+    const itemStructureInput = document.createElement("input")
+    const itemName = document.createElement("span")
+    const itemStructureButton = document.createElement("button")
+    const itemStructureImg = document.createElement("img")
 
-// Changing the input type
-itemStructureInput.type = "checkbox"
+    // Applying classes in the items
+    itemStructureLi.classList.add("listItem")
+    itemStructureDiv.classList.add("textItem")
 
-function addNewItem() {
-    // Showing elements in the list
+    // Searching for the image(trash) to put on the button
+    itemStructureImg.src = "./assets/icons/trash.svg"
+
+    // Changing the input type
+    itemStructureInput.type = "checkbox"
+
     item.append(itemStructureLi)
-
     itemStructureLi.append(itemStructureDiv)
     itemStructureDiv.append(itemStructureInput)
     itemStructureDiv.append(itemName)
@@ -31,11 +34,4 @@ function addNewItem() {
     itemStructureButton.append(itemStructureImg)
 
     itemName.textContent = inputForm.value
-}
-
-
-buttonAddItem.onclick = (event) => {
-    event.preventDefault()
-    
-    addNewItem()
-}
+})
