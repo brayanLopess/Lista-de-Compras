fa8dcd42fa4bec94300ca96b8beb034e9ad91deb
Brayan Lopes <brylps90@outlook.com>
2025-10-12 01:19:28 -0300

    Creating the logical to don`t add null(white space) items

--- scripts.js
+++ scripts.js
@@ -15,29 +15,39 @@ form.addEventListener("submit", (event) => {
     const itemName = document.createElement("span")
     const itemStructureButton = document.createElement("button")
     const itemStructureImg = document.createElement("img")
+    const hasWhiteSpaceRegex = /^\s+$/
+    const value = inputForm.value
+    const valueRegex = value.search(hasWhiteSpaceRegex)
+    
+    if(valueRegex === -1) {
+        // Applying classes in the items
+        itemStructureLi.classList.add("listItem")
+        itemStructureDiv.classList.add("textItem")
+        itemStructureImg.classList.add("trashIcon")
 
-    // Applying classes in the items
-    itemStructureLi.classList.add("listItem")
-    itemStructureDiv.classList.add("textItem")
-    itemStructureImg.classList.add("trashIcon")
+        // Searching for the image(trash) to put on the button
+        itemStructureImg.src = "./assets/icons/trash.svg"
+    
+        // Changing the input type
+        itemStructureInput.type = "checkbox"
+    
+        // Creating the elements
+        item.append(itemStructureLi)
+        itemStructureLi.append(itemStructureDiv)
+        itemStructureDiv.append(itemStructureInput)
+        itemStructureDiv.append(itemName)
+    
+        itemStructureLi.append(itemStructureButton)
+        itemStructureButton.append(itemStructureImg)
+    
+        itemName.textContent = inputForm.value
+        
+        console.log(inputForm.value)
+    
+        // Cleaning text in the input
+        inputForm.value = ""
+    } else {
+        alert("Não é possível adicionar um item vazio, por favor digite o nome do item que você deseja adicionar.")
+    }
 
-    // Searching for the image(trash) to put on the button
-    itemStructureImg.src = "./assets/icons/trash.svg"
-
-    // Changing the input type
-    itemStructureInput.type = "checkbox"
-
-    // Creating the elements
-    item.append(itemStructureLi)
-    itemStructureLi.append(itemStructureDiv)
-    itemStructureDiv.append(itemStructureInput)
-    itemStructureDiv.append(itemName)
-
-    itemStructureLi.append(itemStructureButton)
-    itemStructureButton.append(itemStructureImg)
-
-    itemName.textContent = inputForm.value
-
-    // Cleaning text in the input
-    inputForm.value = ""
 })
